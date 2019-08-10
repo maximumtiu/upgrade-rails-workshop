9967f897ec5a4f3e23f22d118bb7aa51d76ae3d6
Megan Tiu <megantiu@me.com>
2019-08-10 19:08:46 -0400

    Add Posts, rspec, and fix chromedriver deprecation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_222810) do
+ActiveRecord::Schema.define(version: 2019_08_10_223617) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "body"
+    t.datetime "published_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
